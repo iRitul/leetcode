2eace65e5cc01712a0a7099adfd848d1f78ba522
Ritul Mishra <30477955+iRitul@users.noreply.github.com>
2022-10-18 22:49:38 +0530

    Create mergeTwoSortedList.java

---
+++ mergeTwoSortedList.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        ListNode first = (list2.val<list1.val) ? list2 : list1 ;
+        first.next = mergeTwoLists(first.next,first==list1?list2:list1);
+        return first;
+    }
+}
